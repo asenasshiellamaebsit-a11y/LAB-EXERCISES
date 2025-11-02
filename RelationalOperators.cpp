8a54f3e41a0def415e114573e183552af482b0c9
shiella <asenasshiellamae_bsit@plmun.edu.ph>
2025-11-02 19:41:57 +0800

    Implement relational operators demonstration in C++

---
+++ RelationalOperators.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+    int main()
+    {
+        int a = 5;
+        int b = 10;
+        int c = 5;
+
+        cout << "a == b: " << (a == b) << endl;
+        cout << "a != c: " << (a != c) << endl;
+        cout << "a < b: " << (a < b) << endl;
+        cout << "b > c: " << (b > c) << endl;
+        cout << "(a + c) <= b: " << ((a + c) <= b) << endl;
+
+        cout << " 'A' < 'a':  " << ('A' < 'a') << endl;
+        return 0;
+    }
